b1d8c90106211fd3366ade3dec25434bbe78d0fb
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-16 21:51:33 +0900

    create JS compile settings

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+    .sass('resources/sass/app.scss', 'public/css/app.css')
+    .scss('resources/scss/admin/app.scss', 'public/css/app_admin.css')
+    .js('resources/js/app.js', 'public/js/app.js')
+    .js('resources/js/app.js', 'public/js/app_admin.js')
