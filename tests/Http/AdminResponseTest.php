4a22458a555aa4fe578c4c91a1ab482aadd18ee9
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-16 21:21:36 +0900

    create Http-admin-Test

---
+++ tests/Http/AdminResponseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests\Http;
+
+use Tests\TestCase;
+
+class FrontResponseTest extends TestCase
+{
+    /**
+     * @test
+     */
+//    public function トップページのレスポンスは正常である()
+//    {
+//        $this->get('/')->assertStatus(200);
+//    }
+}
