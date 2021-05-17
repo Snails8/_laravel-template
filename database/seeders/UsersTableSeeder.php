849f682403a3f0b8f2d84c3bb69e0e58f54f57ae
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-17 09:53:10 +0900

    create users_table

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name'  => 'たにし',
+                'kana'  => 'タニシ',
+                'email' => 'sample@gmail.com',
+                'email_verified' => Carbon::now(),
+                'password' => bcrypt('password'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+
+        User::factory(20)->create();
+
+        DB::select("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
+    }
+}
