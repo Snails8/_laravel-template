b139e65b2ea57b281117e1bb883a82ac09aa25e1
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-17 09:08:38 +0900

    bootstrap導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,13 +6,13 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix
     .sass('resources/sass/app.scss', 'public/css/app.css')
-    .scss('resources/scss/admin/app.scss', 'public/css/app_admin.css')
+    .sass('resources/sass/admin/app.scss', 'public/css/app_admin.css')
     .js('resources/js/app.js', 'public/js/app.js')
-    .js('resources/js/app.js', 'public/js/app_admin.js')
+    .js('resources/js/admin/app.js', 'public/js/app_admin.js');
