4207c2c43e64c9a5c95e290c4c35b1ad350cd44f
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-23 07:15:21 +0900

    create session-authentication

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController;
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
 
 //Route::middleware('auth:admin')->group(function() {
 //    Route::get()
