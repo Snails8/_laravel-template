9f9dd278105d66b090cd1fa2c3d6e6433c061e06
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-23 16:38:03 +0900

    add LoginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * ログイン処理
+ * Class LoginController
+ * @package App\Http\Controllers\Admin\Auth
+ */
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * ログインフォームの表示
+     * @Method GET
+     * @return View
+     */
+    public function showLoginForm(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard
+     */
+    public function guard()
+    {
+        return Auth::guard('member');
+    }
+}
