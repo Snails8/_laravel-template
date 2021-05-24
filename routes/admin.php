41b1fd20f86b277ac1212e025c5532a2fb3c91fb
S-tanishi <tanichi.u.n.20@gmail.com>
2021-05-25 08:11:11 +0900

    add admin-Home-page

--- routes/admin.php
+++ routes/admin.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\SampleController;
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 
-//Route::middleware('auth:admin')->group(function() {
-//    Route::get()
-//});
+Route::middleware('auth:admin')->group(function() {
+    Route::get('/home', [SampleController::class, 'home']);
+});
