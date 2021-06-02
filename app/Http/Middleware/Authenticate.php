3c559932eb3c2615e390dc5b35a82be4c92d6b17
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-02 21:16:43 +0900

    add middleware setting

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -8,6 +8,7 @@ class Authenticate extends Middleware
 {
     /**
      * ルート内のmiddlewareにauth指定すると先にこのクラスが走る
+     * 認証済みでない場合の処理を書く
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class RedirectIfAuthenticated
 {
     /**
-     * 認証済みでない処理をする
+     * 認証済みの場合、どこにリダイレクトを送るかの処理
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
