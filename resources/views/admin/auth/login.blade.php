dd7023c1b3411f496052b5c87462ec0a50094fbe
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-03 21:21:38 +0900

    add layout

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,18 +1,19 @@
+@include('admin.auth._layouts.layout')
 @section('title', 'ログイン')
 @section('content')
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">{{ __('login') }}</div>
+          <div class="card-header">{{ __('Login') }}</div>
 
           <div class="card-body">
             <form method="POST" action="{{ route('login') }}">
               @csrf
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-mail Address') }}</label>
+                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                 <div class="col-md-6">
-                  <input id="email" type="email" class="form-group @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                   @error('email')
                   <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -54,7 +55,6 @@
                   @endif
                 </div>
               </div>
-
             </form>
           </div>
         </div>
