2ef3ef3d5be4ae88b1f579f0d468230165827978
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-05 16:50:17 +0900

    add CRUD

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\SampleController;
+use App\Http\Controllers\Admin\SampleCrudController;
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
@@ -10,4 +11,8 @@ Route::post('logout',[LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:admin')->group(function() {
     Route::get('/home', [SampleController::class, 'index']);
+
+    Route::resource('crud', SampleCrudController::class)->except([
+        'show'
+    ])->names('admin.crud');
 });
