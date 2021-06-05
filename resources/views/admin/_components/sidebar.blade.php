415b5d72d96af5ccd1f82aec93f8c58d9ce27920
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-05 11:56:16 +0900

    add sidebar

--- resources/views/admin/_components/sidebar.blade.php
+++ resources/views/admin/_components/sidebar.blade.php
@@ -1,25 +1,69 @@
-<nav class="d-none d-md-block bg-light mt-4 sidebar">
-  <div class="sidebar-sticky">
-    <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">物件管理</h6>
-    <ul class="nav flex-column mb-2">
-      <li class="nav-item pl-2">
-        <a class="nav-link" href="">
-          <span data-feather="chevron-right"></span>
-          検索
+<div class="sidebar">
+  <div class="sidebar-inner">
+    <div class="sidebar-logo bg-dark">
+      <div class="peers ai-c fxw-nw">
+        <div class="peer peer-greed">
+          <a class="sidebar-link td-n" href="">
+            <div class="peers ai-c fxw-nw">
+              <div class="peer">
+                <div class="logo">
+                  {{-- <img src="{{ asset('images/logo.png') }}" alt="logo"> --}}
+                </div>
+              </div>
+
+{{--              <div class="peer peer-greed">--}}
+{{--                <h5 class="lh-1 mB-0 logo-text text-white">{{ env('APP_NAME', 'Laravel')}}</h5>--}}
+{{--              </div>--}}
+              
+            </div>
+          </a>
+        </div>
+        <div class="peer">
+          <div class="mobile-toggle sidebar-toggle">
+            <a href="" class="td-n">
+              <i class="ti-arrow-circle-left"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ul class="sidebar-menu scrollable pos-r">
+      <li class="nav-item mT-20">
+        <a class="sidebar-link" href="">
+                <span class="icon-holder">
+                  <i class="c-blue-500 ti-dashboard"></i>
+                </span>
+          <span class="title">Dashboard</span>
         </a>
       </li>
-      <li class="nav-item pl-2">
-        <a class="nav-link" href="">
-          <span data-feather="chevron-right"></span>
-          検索
+      <li class="nav-item dropdown">
+        <a class="dropdown-toggle">
+          <span class="icon-holder"><i class="c-orange-500 ti-email"></i></span>
+          <span class="title">フォーム管理</span>
+          <span class="arrow"><i class="ti-angle-right"></i></span>
+        </a>
+        <ul class="dropdown-menu">
+          <li class="pl-2"><a class='sidebar-link' href="">来店予約</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="sidebar-link" href="">
+                <span class="icon-holder">
+                  <i class="cH-green-500 ti-hummer"></i>
+                </span>
+          <span class="title">施工事例管理</span>
         </a>
       </li>
-      <li class="nav-item pl-2">
-        <a class="nav-link" href="">
-          <span data-feather="chevron-right"></span>
-          依頼
+      <li class="nav-item dropdown">
+        <a class="dropdown-toggle">
+          <span class="icon-holder"><i class="c-blue-500 ti-package"></i></span>
+          <span class="title">マスター管理</span>
+          <span class="arrow"><i class="ti-angle-right"></i></span>
         </a>
+        <ul class="dropdown-menu">
+          <li class="pl-2"><a class='sidebar-link' href="">施工タグ管理</a></li>
+        </ul>
       </li>
     </ul>
   </div>
-</nav>
+</div>
