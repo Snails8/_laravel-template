6fcd72efc64aad771eec6e3fad7c8d347368cb0d
S-tanishi <tanichi.u.n.20@gmail.com>
2021-06-06 19:59:09 +0900

    add detail index page http test sample

--- tests/Http/AdminResponseTest.php
+++ tests/Http/AdminResponseTest.php
@@ -21,4 +21,13 @@ class FrontResponseTest extends TestCase
     {
         $this->get('/admin/login')->assertStatus(200);
     }
+
+    // index detail系のテストサンプル
+//    public function サンプルページのレスポンスは正常である()
+//    {
+//        $hoge = Hoge::query()->select('id')->where('is_public', true)->first();
+//
+//        $this->get('/hoges')->assertStatus(200);
+//        $this->get('/hoges/'. $hoge->id )->assertStatus(200);
+//    }
 }
